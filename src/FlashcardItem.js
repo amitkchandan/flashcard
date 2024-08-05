a2d70fc30b83016a7b28ed1131f4a30228618ff6
Akanksha9033 <happychaudhary123spj@gmail.com>
2024-08-05 10:52:33 +0530

    save

--- src/FlashcardItem.js
+++ src/FlashcardItem.js
@@ -1,6 +1,5 @@
 
 import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
 import './FlashcardItem.css';  // Assuming you have a CSS file for styling
 import pic1 from './assets/pic1.jpg';
 
@@ -8,26 +7,17 @@ const FlashcardItem = ({ question, answer, flipped, toggleFlip }) => {
   return (
     <div className="flashcard-container" onClick={toggleFlip}>
       <div className={`flashcard ${flipped ? 'flipped' : ''}`}>
-        <div className="front">
-          <Card className='cards'>
-            <Card.Body>
-              <Card.Title>Question</Card.Title>
-              <Card.Text>{question}</Card.Text>
-            </Card.Body>
-          </Card>
+        <div className="front cards">
+          <div className="card-title">Question</div>
+          <div className="card-text">{question}</div>
         </div>
-        <div className="back">
-          <Card>
-            <Card.Body>
-              <Card.Img variant="top" src={pic1} style={{ width: '80px' }} />
-              <Card.Title>Answer</Card.Title>
-              <Card.Text>{answer}</Card.Text>
-            </Card.Body>
-          </Card>
+        <div className="back cards">
+          <img src={pic1} className="card-img" alt="related" />
+          <div className="card-title">Answer</div>
+          <div className="card-text">{answer}</div>
         </div>
       </div>
     </div>
   );
 };
-
 export default FlashcardItem;
