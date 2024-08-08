6f1fb895cd8366684d8c05aa2493498214711434
Akanksha9033 <happychaudhary123spj@gmail.com>
2024-08-08 15:13:23 +0530

    save

--- src/FlashcardItem.js
+++ src/FlashcardItem.js
@@ -1,18 +1,18 @@
 
 import React, { useState } from 'react';
 import './FlashcardItem.css';  // Assuming you have a CSS file for styling
-import pic1 from './assets/pic1.jpg';
 
-const FlashcardItem = ({ question, answer, flipped, toggleFlip }) => {
+
+const FlashcardItem = ({ serialNumber, question, answer, flipped, toggleFlip }) => {
   return (
     <div className="flashcard-container" onClick={toggleFlip}>
       <div className={`flashcard ${flipped ? 'flipped' : ''}`}>
         <div className="front cards">
           <div className="card-title">Question</div>
-          <div className="card-text">{question}</div>
+          <div className="card-text">{`Q${serialNumber}.${question}`}</div>
         </div>
         <div className="back cards">
-          <img src={pic1} className="card-img" alt="related" />
+          
           <div className="card-title">Answer</div>
           <div className="card-text">{answer}</div>
         </div>
